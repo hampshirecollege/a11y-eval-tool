1e96fe8ee4b6e97767d5dc5c8721dc18df3e4a76
Vincent Abruzzo <vabruzzo@hampshire.edu>
2016-04-06 14:09:29 -0400

    fix detailed table captions, remove tabs for errors

--- src/components/DetailedPanel.js
+++ src/components/DetailedPanel.js
@@ -12,84 +12,88 @@ export default class DetailedPanel extends Component {
             key={this.props.data.indexOf(item)}
             header={<h3><a href={`//${item.entry}`} target="_blank">{item.entry}</a></h3>}
           >
-            <Tabs defaultActiveKey={1} animation={false}>
-              <Tab
-                tabClassName="tab-error"
-                eventKey={1}
-                title={`Errors: ${item.data.categories.error.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`Errors for ${item.entry}`}
-                  data={item.data.categories.error}
-                  thStyle="th-errors"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-alert"
-                eventKey={2}
-                title={`Alerts: ${item.data.categories.alert.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`Alerts for ${item.entry}`}
-                  data={item.data.categories.alert}
-                  thStyle="th-alerts"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-feature"
-                eventKey={3}
-                title={`Features: ${item.data.categories.feature.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`Features for ${item.entry}`}
-                  data={item.data.categories.feature}
-                  thStyle="th-features"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-structure"
-                eventKey={4}
-                title={`Structure: ${item.data.categories.structure.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`Structure items for ${item.entry}`}
-                  data={item.data.categories.structure}
-                  thStyle="th-structure"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-html5"
-                eventKey={5}
-                title={`HTML5 and ARIA: ${item.data.categories.html5.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`HTML5 and ARIA items for ${item.entry}`}
-                  data={item.data.categories.html5}
-                  thStyle="th-html5"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-contrast"
-                eventKey={6}
-                title={`Contrast: ${item.data.categories.contrast.count}`}
-              >
-                <DetailedTableSingle
-                  caption={`Contrast items for ${item.entry}`}
-                  data={item.data.categories.contrast}
-                  thStyle="th-contrast"
-                />
-              </Tab>
-              <Tab
-                tabClassName="tab-all"
-                eventKey={7}
-                title="View All"
-              >
-                <DetailedTableAll
-                  caption={`All items for ${item.entry}`}
-                  data={item.data}
-                />
-              </Tab>
-            </Tabs>
+            {item.error === '' ?
+              <Tabs defaultActiveKey={1} animation={false}>
+                <Tab
+                  tabClassName="tab-error"
+                  eventKey={1}
+                  title={`Errors: ${item.data.categories.error.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`Errors for ${item.entry}`}
+                    data={item.data.categories.error}
+                    thStyle="th-errors"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-alert"
+                  eventKey={2}
+                  title={`Alerts: ${item.data.categories.alert.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`Alerts for ${item.entry}`}
+                    data={item.data.categories.alert}
+                    thStyle="th-alerts"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-feature"
+                  eventKey={3}
+                  title={`Features: ${item.data.categories.feature.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`Features for ${item.entry}`}
+                    data={item.data.categories.feature}
+                    thStyle="th-features"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-structure"
+                  eventKey={4}
+                  title={`Structure: ${item.data.categories.structure.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`Structure items for ${item.entry}`}
+                    data={item.data.categories.structure}
+                    thStyle="th-structure"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-html5"
+                  eventKey={5}
+                  title={`HTML5 and ARIA: ${item.data.categories.html5.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`HTML5 and ARIA items for ${item.entry}`}
+                    data={item.data.categories.html5}
+                    thStyle="th-html5"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-contrast"
+                  eventKey={6}
+                  title={`Contrast: ${item.data.categories.contrast.count}`}
+                >
+                  <DetailedTableSingle
+                    caption={`Contrast items for ${item.entry}`}
+                    data={item.data.categories.contrast}
+                    thStyle="th-contrast"
+                  />
+                </Tab>
+                <Tab
+                  tabClassName="tab-all"
+                  eventKey={7}
+                  title="View All"
+                >
+                  <DetailedTableAll
+                    entry={item.entry}
+                    data={item.data}
+                  />
+                </Tab>
+              </Tabs>
+              : <span className="row-err">{item.error} unable to scan {item.entry}, check that the URL is correct.
+              </span>
+            }
           </Panel>
         )}
       </Panel>
--- src/components/DetailedTableAll.js
+++ src/components/DetailedTableAll.js
@@ -1,36 +1,36 @@
 import React, { PropTypes } from 'react';
 import { DetailedTableSingle } from '../components';
 
-const DetailedTableAll = ({ data }) => (
+const DetailedTableAll = ({ entry, data }) => (
   <div>
   <p />
     <DetailedTableSingle
-      caption={`Errors for ${data.entry}`}
+      caption={`Errors for ${entry}`}
       data={data.categories.error}
       thStyle="th-errors"
     />
     <DetailedTableSingle
-      caption={`Alerts for ${data.entry}`}
+      caption={`Alerts for ${entry}`}
       data={data.categories.alert}
       thStyle="th-alerts"
     />
     <DetailedTableSingle
-      caption={`Features for ${data.entry}`}
+      caption={`Features for ${entry}`}
       data={data.categories.feature}
       thStyle="th-features"
     />
     <DetailedTableSingle
-      caption={`Structure items for ${data.entry}`}
+      caption={`Structure items for ${entry}`}
       data={data.categories.structure}
       thStyle="th-structure"
     />
     <DetailedTableSingle
-      caption={`HTML5 and ARIA items for ${data.entry}`}
+      caption={`HTML5 and ARIA items for ${entry}`}
       data={data.categories.html5}
       thStyle="th-html5"
     />
     <DetailedTableSingle
-      caption={`Contrast items for ${data.entry}`}
+      caption={`Contrast items for ${entry}`}
       data={data.categories.contrast}
       thStyle="th-contrast"
     />
@@ -38,12 +38,11 @@ const DetailedTableAll = ({ data }) => (
 );
 
 DetailedTableAll.propTypes = {
-  caption: PropTypes.string.isRequired,
+  entry: PropTypes.string.isRequired,
   data: PropTypes.oneOfType([
     React.PropTypes.array,
     React.PropTypes.object,
   ]).isRequired,
-  thStyle: PropTypes.string.isRequired,
 };
 
 export default DetailedTableAll;
