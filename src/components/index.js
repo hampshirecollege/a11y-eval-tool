73d4fa1fd321fad8b80bfd35e68f468491346a12
Vincent Abruzzo <vabruzzo@hampshire.edu>
2016-04-08 22:12:05 -0400

    removed unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
 export { default as SummaryTable } from './SummaryTable';
 export { default as DetailedPanel } from './DetailedPanel';
 export { default as DetailedTableSingle } from './DetailedTableSingle';
