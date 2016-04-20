f8a8442910ae6e54d7a1ca81b8e060b22db44bb1
Vincent Abruzzo <vabruzzo@hampshire.edu>
2016-04-20 09:55:49 -0400

    rm react-a11y, add docs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+/**
+ * External dependencies
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import A11yMain from './containers/A11yMain';
-// import a11y from 'react-a11y';
-import './styles.css';
 
-// if (process.env.NODE_ENV === 'development') a11y(React);
+/**
+ * Internal dependencies
+ */
+import A11yMain from './containers/A11yMain';
+import './styles.css';
 
 ReactDOM.render(
   <A11yMain />,
