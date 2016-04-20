214437d9e29ef26ee8df4b95ab06017b3541d935
Vincent Abruzzo <vabruzzo@hampshire.edu>
2016-04-20 15:40:32 -0400

    fix lint error

--- devServer.js
+++ devServer.js
@@ -16,7 +16,7 @@ app.use(webpackHotMiddleware(compiler));
 app.use(express.static(__dirname));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, function (err) {
